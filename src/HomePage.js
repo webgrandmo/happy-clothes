ea259cd1b5fac06faefce58a0288c866348abb4a
@andriy.trofimov <trofimov.a@hanaboso.com>
2021-11-26 21:01:11 +0200

    Basic homepage structure

---
+++ src/HomePage.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="directory-item">
+        <div className="menu-item">
+          <div className="content">
+            <h1>HATS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-item">
+        <div className="menu-item">
+          <div className="content">
+            <h1>JACKETS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-item">
+        <div className="menu-item">
+          <div className="content">
+            <h1>SNEAKERS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-item">
+        <div className="menu-item">
+          <div className="content">
+            <h1>WOMENS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+      <div className="directory-item">
+        <div className="menu-item">
+          <div className="content">
+            <h1>MENS</h1>
+            <span>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
